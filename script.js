48b00adbafbf88a280171a1d1697afee19f4634b
sametkzlky <sametahmetkzlky@gmail.com>
2023-08-22 14:33:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const icon = document.getElementById("icon");
+
+icon.addEventListener("click", () => {
+  document.body.classList.toggle(`dark-theme`);
+  if (document.body.classList.contains(`dark-theme`)) {
+    icon.classList = "fa-solid fa-sun fa-2xl";
+  } else {
+    icon.classList = `fa-solid fa-moon fa-2xl`;
+  }
+  localStorage.setItem(`theme`, document.body.classList);
+  localStorage.setItem(`icon`, icon.classList);
+});
+
+if (localStorage.getItem(`theme`)) {
+  document.body.classList.add(localStorage.getItem(`theme`));
+  icon.classList = localStorage.getItem('icon');
+}
